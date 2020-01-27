1c85cb5f8e3650eaf74a7bd1130834ab1e665aa1
sulin <sulinwork@163.com>
2020-01-27 13:58:26 +0800

    爬取热门城市

--- JobsSpider/spiders/JobSpider.py
+++ JobsSpider/spiders/JobSpider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class JobspiderSpider(scrapy.Spider):
@@ -8,4 +9,17 @@ class JobspiderSpider(scrapy.Spider):
     start_urls = ['https://www.51job.com']
 
     def parse(self, response):
-        pass
+        # 主页的时候抓取热门城市 确定爬取的城市岗位信息
+        if response.url == 'https://www.51job.com':
+            li_selector = response.xpath("//div[@class='hcity']/div[1]/a")
+            for li in li_selector:
+                city_name = li.xpath("text()")[0].extract()
+                city_url = li.xpath("@href")[0].extract()
+                city_code_regex = ".*com[/](.*)[/]"
+                city_code_match = re.match(city_code_regex, city_url)
+                if city_code_match:
+                    city_code = city_code_match.group(1)
+                    print("name:", city_name, ",code:", city_code)
+                    # 可以往url采集器里面继续扔想要采集的路径
+        else:
+            print("开始爬取数据")
